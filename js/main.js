c81fbca0b69df87978d40e9efea21b5d0d310ce5
morayo_o <morayobusinesses@gmail.com>
2021-06-24 14:08:28 -0700

    Help needed fixing some glitches. My scroll botton isn't working effectively. The JS isn't respondking and I don't know why. It's suppose to scroll to the portfolio section and I don't know how to get it to do that. It's also off center, and I can't seem to center it. Can you help please?

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Nav open tab (Mobile design) ANIMATION
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
@@ -10,6 +11,8 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
+// (Mobile design) Hamburger ANIMATION
+
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
@@ -20,6 +23,7 @@ menuBtn.addEventListener("click", () => {
   });
 });
 
+//STICKY BAR ...
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
@@ -38,3 +42,54 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// Scroll command
+
+$(function() {
+  $('.my-portfolio').on('click', function(e) {
+    e.preventDefault();
+    $('html, main').animate({ scrollTop: $($(this).attr('class')).offset().top}, 500, 'linear');
+  });
+});
+
+// Hero Ends
+
+
+// Portfolio Starts
+
+// Carousel portfolio
+let span = document.getElementsByTagName('span');
+	let project = document.getElementsByClassName('project')
+	let project_page = Math.ceil(project.length/4);
+	let l = 0;
+	let movePer = 25.34;
+	let maxMove = 40;
+	// mobile_view
+	let mob_view = window.matchMedia("(max-width: 768px)");
+	if (mob_view.matches)
+	 {
+	 	movePer = 50.36;
+	 	maxMove = 504;
+	 }
+
+	let right_mover = ()=>{
+		l = l + movePer;
+		if (project == 1){l = 0; }
+		for(const i of project)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.left = '-' + l + '%';
+		}
+
+	}
+	let left_mover = ()=>{
+		l = l - movePer;
+		if (l<=0){l = 0;}
+		for(const i of project){
+			if (project_page>1){
+				i.style.left = '-' + l + '%';
+			}
+		}
+	}
+	span[1].onclick = ()=>{right_mover();}
+	span[0].onclick = ()=>{left_mover();}
